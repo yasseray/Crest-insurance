0c9e9afdc3f47884c320cddd771300197f9cae42
Yasser Arafat M <arafat@perisync.com>
2022-01-29 23:57:45 +0530

    first comment yasser

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -7,6 +7,8 @@ import FooterCrest from '../components/FooterCrest'
 import Premium from '../Views/Premium'
 import Premium2 from '../Views/Premiumstep2'
 import Premium3 from '../Views/Premiumstep3'
+import Premium4 from '../Views/Premiumstep4'
+import Premium5 from '../Views/Premiumstep5'
 
 const Router = () => {
   return (
@@ -17,6 +19,8 @@ const Router = () => {
         <Route exact path='/1' render={() => { return <Premium/> }} />
         <Route exact path='/2' render={() => { return <Premium2/>  }} />
         <Route exact path='/3' render={() => { return <Premium3/> }} />
+        <Route exact path='/4' render={() => { return <Premium4/> }} />
+        <Route exact path='/5' render={() => { return <Premium5/> }} />
       </Switch>
       <FooterCrest/>
     </AppRouter>
